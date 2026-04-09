bb0c099a20f30c0f63b6c4e2abef686187a24194
cubexgani <mroybros30@gmail.com>
2026-04-09 12:20:30 +0530

    Add variable

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int n = 67;
+        System.out.println("The value of n is " + n);
     }
 }
