63eae104e72e67a7adf6a9efcf4fb30217a3953a
cubexgani <mroybros30@gmail.com>
2026-04-09 13:15:27 +0530

    Add palindrome function

--- Main.java
+++ Main.java
@@ -3,5 +3,16 @@ public class Main {
         System.out.println("Hello world!");
         int n = 67;
         System.out.println("The value of n is " + n);
+        System.out.println(palindrome(n));
+    }
+    static boolean palindrome(int s) {
+        int cp = s, rev = 0;
+        while (s > 0) {
+            int d = s % 10;
+            rev = rev * 10 + d;
+            s /= 10;
+        }
+        if (rev == cp) return true;
+        return false;
     }
 }
