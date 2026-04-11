88dcecfa452c705839f42ae190815fc208d1ab3a
mehulroy <mrcodes06@gmail.com>
2026-04-11 14:00:17 +0530

    Add toString method for printing

--- Sphere.java
+++ Sphere.java
@@ -9,8 +9,12 @@ public class Sphere {
     double surfaceArea() {
         return 4 * Math.PI * radius * radius;
     }
+    public String toString() {
+        return String.format("Sphere [ radius = %.2f ]", radius);
+    }
     public static void main(String[] args) {
         Sphere sp = new Sphere(4);
+        System.out.println(sp);
         System.out.println("Sphere volume = " + sp.volume());
         System.out.println("Sphere surface area = " + sp.surfaceArea());
     }
