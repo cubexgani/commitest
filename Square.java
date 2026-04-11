dc7ce7cb051a74f9d4f64a855bcbf83c961b331c
mehulroy <mrcodes06@gmail.com>
2026-04-11 14:05:02 +0530

    Add perimeter method to class

--- Square.java
+++ Square.java
@@ -6,11 +6,16 @@ public class Square {
     int area() {
         return side * side;
     }
+    int perimeter() {
+        return 4 * side;
+    }
     public String toString() {
-        return "Square [side = " + side + " ]";
+        return "Square [ side = " + side + " ]";
     }
     public static void main(String[] args) {
         Square sq = new Square(4);
         System.out.println(sq);
+        System.out.println("Perimeter: " + sq.perimeter());
+        System.out.println("Area: " + sq.area());
     }
 }
